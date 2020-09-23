9ce45d1e80b59cd8d90baaf6042ea3084c4935b9
Holmze <704062177@qq.com>
2020-09-23 21:31:08 +0800

    update

--- ch5：LDA/chapter5_3.py
+++ ch5：LDA/chapter5_3.py
@@ -1,3 +1,148 @@
+# import cv2 as cv
+# import os
+# from PIL import Image
+
+# path = 'D:\\Course\机器学习\\MachineLearning-FZU\\ch5：LDA\\LDA_dataset_17flowers'
+
+# TrainFiles = os.listdir(path)  # 遍历每个子文件夹
+# # 计算有几个文件(图片命名都是以 序号.jpg方式)
+# Train_Number = len(TrainFiles)  # 子文件夹个数
+# for k in range(0, Train_Number):
+#     Trainneed = os.listdir(path + '\\' + TrainFiles[k])  # 遍历每个子文件夹里的每张图片
+#     Trainneednumber = len(Trainneed)  # 每个子文件里的图片个数
+#     print(Trainneednumber)
+#     for i in range(0, Trainneednumber):
+#         # image = cv.imread(path + '\\' + TrainFiles[k] + '\\' + Trainneed[i])
+#         image = Image.open(path + '\\' + TrainFiles[k] + '\\' + Trainneed[i])
+#         # print(path + '\\' + TrainFiles[k] + '\\' + Trainneed[i])
+#         # print(image)
+#         image.resize((200, 180))
+#         if i == 1:
+#             image.show()
+        
+#         # print(image)
+#         # cv.imwrite(path + '\\' + TrainFiles[k] + '\\' + Trainneed[i], p)
+# import cv2 as cv
+# import os
+# from PIL import Image
+
+# path = 'D:\\Course\机器学习\\MachineLearning-FZU\\ch5：LDA\\LDA_dataset_17flowers'
+# save_path = 'D:\\Course\机器学习\\MachineLearning-FZU\\ch5：LDA\\flowers'
+
+# TrainImages = os.listdir(path)  # 遍历每个子文件夹
+# # 计算有几个文件(图片命名都是以 序号.jpg方式)   TrainImages[0] = image_0001.jpg
+# Train_Number = len(TrainImages)  # 所有图片个数
+
+# # os.mkdir(save_path + '/flowers_new')
+
+# for i in range(1, 18):  # 包头不包尾
+#     os.mkdir(save_path + str(range(1, 18)[i - 1]))
+# for k in range(0, Train_Number):
+#     for i in range(1, 18):
+#         image = Image.open(path + '/' + TrainImages[k])
+#         if k//80+1 == range(1, 18)[i - 1]:
+#             image.save(save_path + str(k//80+1) + '/' + TrainImages[k], image)
+#             # print(TrainImages[k], k//80+1)
+# import matplotlib.pyplot as plt
+# import os
+# import cv2 as cv
+# import numpy as np
+# from sklearn.discriminant_analysis import LinearDiscriminantAnalysis
+# from PIL import Image
+# from clustering_performance import clusteringMetrics1
+# from sklearn.decomposition import PCA
+# from sklearn.neighbors import KNeighborsClassifier
+# from sklearn.model_selection import train_test_split
+
+# path = 'D:\\Course\机器学习\\MachineLearning-FZU\\ch5：LDA\\LDA_dataset_17flowers'
+# image_SIZE = 40
+# IMAGE_COLUMN = 10  # 列
+# IMAGE_ROW = 10  # 行
+# to_image = Image.new('RGB', (IMAGE_COLUMN * image_SIZE, IMAGE_ROW * image_SIZE))
+
+
+# def createDatabase(path):
+#     # 查看路径下所有文件
+#     TrainFiles = os.listdir(path)  # 遍历每个子文件夹
+#     # 计算有几个文件(图片命名都是以 序号.jpg方式)
+#     Train_Number = len(TrainFiles)  # 子文件夹个数
+#     X_train = []
+#     y_train = []
+#     # 把所有图片转为1维并存入T中
+#     for k in range(0, Train_Number):
+#         Trainneed = os.listdir(path + '/' + TrainFiles[k])  # 遍历每个子文件夹里的每张图片
+#         Trainneednumber = len(Trainneed)  # 每个子文件里的图片个数
+#         for i in range(0, Trainneednumber):
+#             img = Image.open(path + '/' + TrainFiles[k] + '/' + Trainneed[i]).resize((image_SIZE, image_SIZE), Image.ANTIALIAS)
+#             to_image.paste(img, (i * image_SIZE, k * image_SIZE))  # 把读出来的图贴到figure上
+#             image = Image.open(path + '/' + TrainFiles[k] + '/' + Trainneed[i])  # 数据类型转换
+#             # image = cv.cvtColor(image, cv.COLOR_RGB2GRAY)  # RGB变成灰度图
+#             imzge = image.resize((666*500,1),1)
+#             X_train.append(image)
+#             y_train.append(k)
+#             if i==1:
+#                 print(image)
+#     X_train = np.array(X_train)
+#     y_train = np.array(y_train)
+#     return X_train, y_train
+
+
+# X_train, y_train = createDatabase(path)
+# # print(X_train.shape)
+# X_train = X_train.reshape(X_train.shape[0], 180*200)
+# # print(X_train)
+# X_train, X_test, y_train, y_test = \
+#     train_test_split(X_train, y_train, test_size=0.2, random_state=22)
+
+# plt.figure()
+# plt.imshow(to_image)
+# plt.show()
+
+# A_PCA = []
+# A_LDA = []
+# for i in range(1, 11):
+#     # PCA + KNN
+#     pca = PCA(n_components=i).fit(X_train)  # pca模型训练
+#     # 将输入数据投影到特征面正交基上
+#     X_train_pca = pca.transform(X_train)
+#     X_test_pca = pca.transform(X_test)
+#     knn = KNeighborsClassifier()
+#     knn.fit(X_train_pca, y_train)
+#     y_sample = knn.predict(X_test_pca)
+#     ACC_PCA = clusteringMetrics1(y_test, y_sample)
+#     A_PCA.append(ACC_PCA)
+#     # LDA + KNN
+#     lda = LinearDiscriminantAnalysis(n_components=i).fit(X_train, y_train)  # lda模型训练
+#     # 将输入数据投影到特征面正交基上
+#     X_train_lda = lda.transform(X_train)
+#     X_test_lda = lda.transform(X_test)
+#     knn = KNeighborsClassifier()
+#     knn.fit(X_train_lda, y_train)
+#     y_sample = knn.predict(X_test_lda)
+#     ACC_LDA = clusteringMetrics1(y_test, y_sample)
+#     A_LDA.append(ACC_LDA)
+
+# # 画柱状图
+# fig, ax = plt.subplots()
+# bar_width = 0.35
+# opacity = 0.6  # 不透明度
+# index = np.arange(10)
+# ax.set_xticks(index + bar_width / 2)
+
+# cylinder1 = ax.bar(index, A_PCA, bar_width, alpha=opacity, color='b', label='PCA')
+# cylinder2 = ax.bar(index + bar_width, A_LDA, bar_width, alpha=opacity, color='g', label='LDA')
+
+# label = []  # 横坐标标签
+# for j in range(1, 11):
+#     label.append(j)
+# ax.set_xticklabels(label)
+
+# plt.ylabel('ACC')
+# plt.xlabel('Component')
+# ax.legend()  # 图例标签
+
+# plt.show()
+
 import matplotlib.pyplot as plt
 import numpy as np
 import pandas as pd
@@ -9,23 +154,28 @@ from sklearn.neighbors import KNeighborsClassifier
 from sklearn.model_selection import train_test_split
 from sklearn.metrics import accuracy_score
 import random
-
+import time
 
 def load_Image_Into_Matrix(path):
+    time_start = time.time()
     cnt = 0
+    print(os.listdir(path))
     for img_filefolder in os.listdir(path):
         cnt += 1
+        print(cnt)
         img_matrix = np.zeros((1,350*350*3))
         target_matrix = np.zeros((1,1))
         for img_file in os.listdir(path + img_filefolder):
-            img = Image.open(path + img_filefolder + "/" + img_file)
+            print("read",path + img_filefolder + "\\" + img_file,"at",time.time()-time_start)
+            img = Image.open(path + img_filefolder + "\\" + img_file)
+            # print(img.size[0],img.size[1])
             if (img.size[0]<img.size[1]):
                 img = img.rotate(90)
             img = img.crop((100,100,450,450))
             img = np.asarray(img).reshape(1,350*350*3)
             img_matrix = np.vstack((img_matrix,img))
             target_matrix = np.vstack((target_matrix,np.asarray(cnt)))
-        break
+        # break
     img_matrix = np.delete(img_matrix,0,0)
     target_matrix = np.delete(target_matrix,0,0)
     print("complete!")
@@ -41,12 +191,14 @@ def load_Image_Into_Matrix(path):
 # pre_y = knn.predict(test_data)
 # print(accuracy_score(test_y,pre_y))
 
-img_matrix,target_matrix = load_Image_Into_Matrix('D:\Course\机器学习\MachineLearning-FZU\ch5：LDA\LDA_dataset_17flowers/17flowers/')
+img_matrix,target_matrix = load_Image_Into_Matrix('D:\\Course\机器学习\\MachineLearning-FZU\\ch5：LDA\\LDA_dataset_17flowers\\')
 lda = LinearDiscriminantAnalysis(n_components=10)
+# print(n_features, n_classes)
 knn = KNeighborsClassifier(n_neighbors=10)
 train_data,test_data,train_y,test_y = train_test_split(img_matrix,target_matrix,test_size=0.2)
 data = lda.fit_transform(train_data,train_y)
 test_data = lda.transform(test_data)
+print(data,train_y)
 knn.fit(data,train_y)
 pre_y = knn.predict(test_data)
 print(accuracy_score(test_y,pre_y))
--- ch5：LDA/proj5_1.py
+++ ch5：LDA/proj5_1.py
@@ -37,7 +37,6 @@ from sklearn.discriminant_analysis import LinearDiscriminantAnalysis
 from sklearn.model_selection import train_test_split
 from clustering_performance import clusteringMetrics1  # 这里修改一下老师代码的返回值，只返回要用到的ACC
 
-
 digits = load_digits()
 X_train, X_test, y_train, y_test = train_test_split(digits.data, digits.target, test_size=0.2, random_state=22)
 
@@ -110,81 +109,3 @@ for j in range(1, 10):
 ax.set_xticklabels(label)
 ax.legend()  # 图例标签
 plt.show()
-
-
-
-# ##########################################
-# #sklearn中调用PCA函数进行相关的训练和计算（自定义数据）
-# import  numpy as np
-# import matplotlib.pyplot as plt
-# x=np.empty((100,2))
-# x[:,0]=np.random.uniform(0.0,100.0,size=100)
-# x[:,1]=0.75*x[:,0]+3.0*np.random.normal(0,3,size=100)
-# plt.figure()
-# plt.scatter(x[:,0],x[:,1])
-# plt.show()
-# from sklearn.decomposition import PCA    #在sklearn中调用PCA机器学习算法
-# pca=PCA(n_components=1)                  #定义所需要分析主成分的个数n
-# pca.fit(x)                               #对基础数据集进行相关的计算，求取相应的主成分
-# print(pca.components_)                    #输出相应的n个主成分的单位向量方向
-# x_reduction=pca.transform(x)                #进行数据的降维
-# x_restore=pca.inverse_transform(x_reduction)       #对降维数据进行相关的恢复工作
-# plt.figure()
-# plt.scatter(x[:,0],x[:,1],color="g")
-# plt.scatter(x_restore[:,0],x_restore[:,1],color="r")
-# plt.show()
-
-# #sklearn中利用手写字体的数据集进行实际的PCA算法
-# #1-1导入相应的库函数
-# from sklearn import datasets
-# d=datasets.load_digits()
-# x=d.data
-# y=d.target
-# from sklearn.model_selection import train_test_split
-# x_train,x_test,y_train,y_test=train_test_split(x,y,random_state=666)
-# print(x_train.shape)
-# from sklearn.neighbors import KNeighborsClassifier
-# knn=KNeighborsClassifier()
-# knn.fit(x_train,y_train)
-# print(knn.score(x_test,y_test))
-
-# #1-2对于64维的原始数据进行降维，降到2维数据
-# pca1=PCA(n_components=2)
-# pca1.fit(x_train)
-# x_train_re=pca1.transform(x_train)    #对于训练数据和测试数据进行降维到二维数据
-# x_test_re=pca1.transform(x_test)
-# knn1=KNeighborsClassifier()
-# knn1.fit(x_train_re,y_train)             #再对降维到的二维数据进行KNN算法的训练和测试准确度
-# print(knn1.score(x_test_re,y_test))
-# print(pca1.explained_variance_ratio_)
-
-# #1-3-1对于训练数据和测试数据进行降维到64维数据，维度不变
-# pca2=PCA(n_components=64)             #对于训练数据和测试数据进行降维到64维数据，维度不变
-# pca2.fit(x_train)
-# x_train_re=pca2.transform(x_train)
-# x_test_re=pca2.transform(x_test)
-# knn1=KNeighborsClassifier()
-# knn1.fit(x_train_re,y_train)
-# print(knn1.score(x_test_re,y_test))
-# print(pca2.explained_variance_ratio_)           #输出各个主成分对于整体数据的方差的体现比例
-# #1-3-2输出前n个主成分所能够反映的数据的特征权重
-# plt.figure()
-# plt.plot([i for i in range(x.shape[1])],[np.sum(pca2.explained_variance_ratio_[:i+1]) for i in range(x.shape[1])])
-# plt.show()
-# #1-4PCA(a)括号里面的a为0-1的数字，表示输出满足能够反映原始数据比重为a时的最低维度时的PCA，之后进行训练和分类会提高计算的效率5-10倍，但是分类准确度基本相差无几，可以用准确度来换取计算的效率
-# pca3=PCA(0.95)
-# pca3.fit(x_train)
-# print(pca3.n_components_)  #输出此时降到的数据维度
-# x_train_re1=pca3.transform(x_train)
-# x_test_re1=pca3.transform(x_test)
-# knn2=KNeighborsClassifier()
-# knn2.fit(x_train_re1,y_train)
-# print(knn2.score(x_test_re1,y_test))
-# #1-5对于64维度数据进行降维到二维数据，之后进行数据的可视化，可以对于不同的分类结果进行查询和可视化区分
-# pca1=PCA(n_components=2)
-# pca1.fit(x)
-# x_re=pca1.transform(x)
-# plt.figure()
-# for i in range(10):
-#     plt.scatter(x_re[y==i,0],x_re[y==i,1])
-# plt.show()
